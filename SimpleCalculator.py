bce0e667c2229ec1e5e660be3a5cdefba317d1a8
chamarac99 <151674835+chamarac99@users.noreply.github.com>
2024-01-01 20:36:55 +0530

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,15 @@
+
+n1 =int(input("Enter number 1: "))
+n2 =int(input("Enter number 2: "))
+op =input("Enter the Operation: ")
+if op == '+' :
+    print(n1+n2)
+elif op == '-' :
+    print(n1-n2)
+elif op == '*' :
+    print(n1*n2)
+elif op == '/' :
+    print(n1/n2)
+else :
+    print("Enter correct operation")
+
